f31260d139690e5f8c1cc37eed2eb63e5ef6843f
Marco Rodríguez <marcorodrguez@pop-os.localdomain>
2023-09-11 17:12:21 -0600

    Q2

--- quizData.js
+++ quizData.js
@@ -1,119 +1,100 @@
 // quizData.js
-var eq1 = '$$(x_{\\alpha})_{\\alpha\\in\\ I}$$';
-var eq2 = '$$x_{\\alpha} \\in\\ X$$';
-var eq3 = '$$\\alpha\\ \\in\\ I$$';
-var eq4 = '$$T:X \\to\\ Y$$';
-var eq5 = '$$\\forall\\ x_{1}, x_{2} \\in\\ D(T)$$';
-var eq6 = '$$x_{1} \\neq\\ x_{2} \\implies\\ Tx_{1} \\neq\\ Tx_{2}$$';
-var eq7 = '$$\\tau\\ = \\{ U_i \\mid\\ i \\in\\ I \\} $$';
-
+const eq1 = '$$\\text{Sea }  x_{0} \\in\\ X \\text{ y } r>0. \\text{ Definimos:}$$';
+const eq2 = '$$B(x_{0};r) = \\{ x \\in\\ X \\, | \\, d(x,x_{0}) < r \\}$$';
+const eq3 = '$$B(x_{0};r) = \\{ x \\in\\ X \\, | \\, d(x,x_{0}) \\leq\\ r \\}$$';
+const eq4 = '$$B(x_{0};r) = \\{ x \\in\\ X \\, | \\, d(x,x_{0}) = r \\}$$';
+const eq5 = '$$\\text{Sea }  A \\subset\\ X \\text{ y } x \\in\\ X, \\text{ tal que }$$';
+const eq6 = '$$B(x; \\epsilon) \\cap\\ A \\neq\\ \\emptyset\\ \\, \\forall\\ \\epsilon\\ >0.$$';
+const eq7 = '$$\\text{ Entonces se dice que } x$$';
+const eq8 = '$$\\text{Sea }  A \\subset\\ X. \\text{ La siguiente definición corresponde a }$$';
+const eq9 = '$$\\partial\\ A := \\{ x \\in\\ X \\, | \\, \\text{ es un punto frontera de } A \\}$$';
+const eq10 = '$$\\text{Para todo } n \\in\\ \\text{ℕ} \\text{ y } A \\subset\\ \\text{ℝ}^{n} \\text{ con la topología Euclideana. }$$';
+const eq11 = '$$A \\text{ es compacto si y sólo si, } A \\text{ es cerrado y acotado.}$$'
 var quizQuestions = [
+  {
+    question: "El siguiente teorema:  "+eq10+" "+eq11+" corresponde a:",
+    options: [
+        "Teorema espacio métrico segundo numerable",
+        "Teorema de Heine-Borel",
+        "Teorema continuidad topológica",
+        "Teorema continuidad en espacios métricos"
+    ],
+    correctAnswer: 1,
+    feedback2: " El teorema de Heine-Borel habla de subconjuntos compactos sobre campos reales o complejos",
+    feedback: " Este teorema fue fundamental en el curso de cálculo vectorial (Cálculo 3)"
+  },
     {
-      question: "La expresión {x | x ∈ A o x ∈ B} corresponde al conjunto:",
+      question: " "+eq1+" "+eq2+" ",
       //question:  eq2,
-        options: ["Al conjunto Unión A ∪ B", "Al conjunto Intersección A ∩ B", "Al conjunto Diferencia de A \\ B", "Al conjunto vacío ∅"],
+        options: ["Como la Bola abierta con centro en x₀ y radio r", "Como la Bola cerrada con centro en x₀ y radio r", "Como la Esfera con centro en x₀ y radio r", "Como el conjunto vacío ∅"],
         correctAnswer: 0,
-        feedback2: "Es el conjunto unión.", 
-        feedback: "El conector lógico de la disyunción o nos habla de la unión de conjuntos."
+        feedback2: " Esa es la definición de Bola abierta", 
+        feedback: " Observa bien las desigualdades"
     },
     {
-      question: "La expresión {x | x ∈ A y x ∈ B} corresponde al conjunto:",
-      options: ["Al conjunto Unión A ∪ B", "Al conjunto Intersección A ∩ B", "Al conjunto Diferencia de A \\ B", "Al conjunto vacío ∅"],
-      correctAnswer: 1,
-      feedback2: "Es el conjunto intersección.",
-      feedback: "El conector lógico de la conjunción y nos habla de la intersección de conjuntos."
+      question: " "+eq1+" "+eq3+" ",
+      //question:  eq2,
+        options: ["Como la Bola abierta con centro en x₀ y radio r", "Como la Bola cerrada con centro en x₀ y radio r", "Como la Esfera con centro en x₀ y radio r", "Como el conjunto vacío ∅"],
+        correctAnswer: 1,
+        feedback2: " Esa es la definición de Bola cerrada", 
+        feedback: " Observa bien las desigualdades"
     },
     {
-      question: "La expresión {x | x ∈ A y x ∉ B} corresponde al conjunto:",
-      options: ["Al conjunto Unión A ∪ B", "Al conjunto Intersección A ∩ B", "Al conjunto Diferencia de A \\ B", "Al conjunto vacío ∅"],
-      correctAnswer: 2,
-      feedback2: "Es el conjunto diferencia o resta de conjuntos.",
-      feedback: "Si se tiene una conjunción con el conector y pero un elemento no está en uno de los conjuntos ¿qué operación entre conjuntos hace que esto ocurra?"
+      question: " "+eq1+" "+eq4+" ",
+      //question:  eq2,
+        options: ["Como la Bola abierta con centro en x₀ y radio r", "Como la Bola cerrada con centro en x₀ y radio r", "Como la Esfera con centro en x₀ y radio r", "Como el conjunto vacío ∅"],
+        correctAnswer: 2,
+        feedback2: " Esa es la definición de esfera", 
+        feedback: " Observa bien las desigualdades"
     },
     {
-      question: "La expresión A - (B U C) = (A - B) U (A - C), es equivalente a la de una ley para operaciones con el complemento de un conjunto ¿de qué ley o leyes se trata?",
+      question: " "+eq5+" "+eq6+" "+eq7+"",
       options: [
-          "Ley Distributiva",
-          "Ley Asociativa",
-          "Leyes de Morgan",
-          "Ley conmutativa"
+          "Es un punto frontera o de acumulación de A",
+          "Es la frontera de A",
+          "Es la cerradura de A",
+          "Es un punto interior de A"
       ],
-      correctAnswer: 2,
-      feedback2: "Esta es una expresión equivalente para diferencias de conjuntos de las Leyes de Morgan.",
-      feedback: "En clase mencionamos que existe una expresión equivalente a la unión e intersección de conjuntos complemento si se trabaja con diferencias de conjuntos, de hecho son dos leyes, las leyes de..."
+      correctAnswer: 0,
+      feedback2: " Es la definición de punto de acumulación",
+      feedback: " :("
     },
     {
       // question: "Definimos una familia de elementos de un conjunto X como todos los elementos tales que: (X_α)_(α ∈ I) con x_α ∈ X la imagen de α ∈ I. En donde I mapea elementos a X. Esto lo vuelve diferente a simplemente tomar un subconjunto de X, dado que",
-      question: "Definimos una familia de elementos de un conjunto X como todos los elementos tales que: "+eq1+" con "+eq2+" la imagen de "+eq3+" En donde I mapea elementos a X. Esto lo vuelve diferente a simplemente tomar un subconjunto de X, dado que",
+      question: " "+eq8+" "+eq9+"",
       options: [
-          "En las familias no está bien definida la unión e intersección de conjuntos",
-          "Podemos realizar operaciones de unión e intersección de familias",
-          "Bajo el mapeo de I con X, se preservan estructuras algebraicas.",
-          "Pensar en subconjuntos y familias es equivalente."
+          "Punto frontera o de acumulación de A",
+          "La frontera de A",
+          "Es la cerradura de A",
+          "Es un punto interior de A"
       ],
-      correctAnswer: 2,
-      feedback2: "La importancia de usar familias es que se preserven las estructuras de cada subconjunto.",
-      feedback: "El conjunto numerable I es el centro de esta definición ¿para qué definimos estos mapeos entre conjuntos?"
+      correctAnswer: 1,
+      feedback2: "Es la definición de la frontera de un conjunto",
+      feedback: " :("
     },
   {
-    question: "Al definir mapeos o funciones, se considera al conjunto A como el dominio del mapeo, que bajo la aplicación se denota T: D(T) -> Y, así como al rango R(T) como el conjunto de todas las imágenes. ¿Cómo se llama el conjunto de todas las x en D(T) tales que Tx = y0?",
+    question: "Completa la frase: ``Las bolas abiertas son ------------------------- y las bolas cerradas son -------------------''",
     options: [
-        "Imagen directa de una función o mapeo",
-        "Imagen inversa de una función o mapeo",
-        "Dominio de una función o mapeo",
-        "Rango de una función o mapeo"
+        "Intervalos",
+        "Conjuntos abiertos y conjuntos cerrados",
+        "Bolas semi abiertas y semi cerradas",
+        "Bases de vecindades"
     ],
     correctAnswer: 1,
-    feedback2: "Se trata de la definición de la imagen inversa.",
-    feedback: "Observa bien los subconjuntos definidos como el Dominio, Rango e imagen cuando hacemos un mapeo o función ¿hacia donde apunta el mapeo?"
+    feedback2: " Las bolas heredan propiedades topológicas",
+    feedback: " Lee con cuidado"
 },
 {
-    question: "Se dice que un mapeo "+eq4+" es  ______  si al mapear D(T) sobre Y, queda R(T) = Y. ¿Cómo se denomina este tipo de mapeo?",
+    question: "Completa la frase: ``Todos los espacios métricos son --------------, la -------- define la noción de ---------, pero no todos los espacios topológicos son métricos.",
     options: [
-        "Función o mapeo biyectiva",
-        "Función o mapeo uno-a-uno",
-        "Función o mapeo inyectiva",
-        "Función o mapeo sobreyectiva"
+        "normados, norma, vector",
+        "topológicos, métrica, abierto",
+        "vectoriales, topología, intervalo",
+        "topológicos, métrica, ángulo"
     ],
-    correctAnswer: 3,
-    feedback2: "Aquí no podían fallar, nomamn.",
-    feedback: "¡Tu sabes esta! en la definición está oculta la respuesta."
-},
-{
-  question: "Se dice que un mapeo "+eq4+" es  ______  si "+eq5+" se cumple "+eq6+"",
-  options: [
-      "Función o mapeo biyectiva",
-      "Función o mapeo uno-a-uno",
-      "Función o mapeo suprayectiva",
-      "Función o mapeo sobreyectiva"
-  ],
-  correctAnswer: 1,
-  feedback2: "Aquí no podían fallar, nomamn.",
-  feedback: "¡Tu sabes esta! en la definición está oculta la respuesta."
-},
-{
-  question: "Sea X un conjunto y "+eq7+" una colección de subconjuntos de X tal que contiene al vacío y X, es cerrada bajo la unión arbitraria de sus elementos y cerrada bajo la intersección finita de sus elementos. Entonces (X, τ) es un ... ",
-  options: [
-      "Espacio Métrico",
-      "Espacio Topológico",
-      "Espacio Normado",
-      "Espacio de Medida"
-  ],
-  correctAnswer: 1,
-  feedback2: "Es el inicio de la definición de un espacio topológico.",
-  feedback: "Si observas al mapeo τ ¿no era este mapeo lo que dotaba de una topología al conjunto X?"
-},
-{
-  question: "Sea X cualquier conjunto y d: X x X -> ℝ una función tal que es no negativa, satisface la desigualdad del triángulo, cero si y sólo si los dos argumentos son iguales y simétrica. Entonces (X, d) corresponde a la definición de un:",
-  options: [
-      "Espacio Métrico",
-      "Espacio Topológico",
-      "Espacio Normado",
-      "Espacio de Medida"
-  ],
-  correctAnswer: 0,
-  feedback2: "Es el inicio de la definición de un espacio métrico.",
-  feedback: "Si observas a la función d ¿a esta no le llamámos la métrica?"
+    correctAnswer: 1,
+    feedback2: " Los espacios métricos son espacios topológicos, aunque no ocurre en sentido contrario",
+    feedback: " Lee con cuidado"
 }
 
   ];
